40fbde57559da78f819f0281b6b7b2677fa2f53f
IzharMohammed <izharmohammed21@gmail.com>
2025-07-19 18:29:17 +0530

    Two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpAmt = Math.floor(Math.sqrt(breaks.length - 1));
 
+    let j = jumpAmt
+    for (; j < breaks.length; j += jumpAmt) {
+        if (breaks[j]) {
+            break;
+        }
+    }
+
+    j -= jumpAmt;
+
+    for (; j < breaks.length; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+
+    return -1;
 }
