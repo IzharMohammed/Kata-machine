591fdb96a8275684562bb1456be38e444850838b
IzharMohammed <izharmohammed21@gmail.com>
2025-07-19 15:58:44 +0530

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length - 1;
 
+    while (low <= high) {
+        const middle = Math.floor(low + (high - low) / 2);
+        const v = haystack[middle];
+
+        if (v === needle) {
+            return true
+        } else if (v > needle) {
+            high = middle - 1;
+        } else {
+            low = middle + 1;
+        }
+    }
+
+    return false;
 }
